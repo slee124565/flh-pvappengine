faca9ca7219d7bced383c721c1dee84d3383788d
Lee Shiueh <lee.shiueh@gmail.com>
2015-12-20 11:23:23 +0800

    testing backup

--- pvi/h5/controller.py
+++ pvi/h5/controller.py
@@ -14,24 +14,26 @@ logger.addHandler(handler)
 import minimalmodbus
 instr = minimalmodbus.Instrument('/dev/ttyUSB0',2)    
 instr.serial.baudrate = 9600    
+instr.serial.timeout = 0.1
 instr.debug=True
 
 def modbus_input_register_read(reg_addr):
-    try:
-        val = instr.read_register(int(reg_addr)-1,functioncode=4)
-        return val
-    except:
-        raise Exception('modbus read register with address %s failed' % reg_addr)
+    val = instr.read_register(int(reg_addr)-1,functioncode=4)
+    return val
 
 def get_register_value_by_name(reg_name):
     reg_entry = INPUT_REGISTER.get(reg_name)
     if reg_entry:
         reg_addr = reg_entry[0]
         reg_len = reg_entry[2]
+        reg_value = 0
+        logger.debug('reading register: ' + reg_name + ',' + str(reg_addr) + ',' + str(reg_len))
         if reg_len == 1:
             reg_value = int(modbus_input_register_read(reg_addr))
+            logger.debug('1st register vale: ' + str(reg_value))
         elif reg_len == 2:
             reg_value = int(modbus_input_register_read(reg_addr))
+            logger.debug('1st register vale: ' + str(reg_value))
             reg_value += (int(modbus_input_register_read(int(reg_addr)+1))*0x10000)
         else:
             raise Exception('register length ' + str(reg_len) + ' not implement!')
@@ -54,6 +56,6 @@ def save_all_pvi_input_register_value():
             reg_data.save()
             logger.info('saved,'+reg_name+','+str(reg_value))
         except Exception as e:
-            logger.error('save pvi input register vaule error.', exc_info=True)
+            logger.error('save pvi input register value error.', exc_info=True)
             
 

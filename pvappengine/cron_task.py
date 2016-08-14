f5e3048f12ebe3e05857a8c1924186cb7f1015b8
lee_shiueh <lee_shiueh@lee-home-mac>
2016-08-14 10:50:16 +0800

    reverse data sample order for pvi and accuweather

--- pvappengine/cron_task.py
+++ pvappengine/cron_task.py
@@ -18,17 +18,17 @@ from dbconfig.views import get_app_json_db_config
 import accuweather
     
 try:
-    from pvi.h5 import controller as h5_controller
-    h5_controller.check_n_set_measurement_index()
-    h5_controller.save_all_pvi_input_register_value()
-    logger.info('h5_controller.save_all_pvi_input_register_value executed')
-
     from accuweather.models import CurrConditions
     accu_dbconfig = get_app_json_db_config('accuweather', accuweather.DEFAULT_DB_CONFIG)
     CurrConditions.save_current_location_condition(accu_dbconfig['locationkey'],
                                                    accu_dbconfig['apikey'])
     logger.info('CurrConditions.save_current_location_condition executed')
     
+    from pvi.h5 import controller as h5_controller
+    h5_controller.check_n_set_measurement_index()
+    h5_controller.save_all_pvi_input_register_value()
+    logger.info('h5_controller.save_all_pvi_input_register_value executed')
+
     import pvi.views
     import accuweather.views
     pvi.views.clear_expired_records()

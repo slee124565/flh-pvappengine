7c1d373f792db92f23e87f37bc7964c8e32edda9
Lee Shiueh <lee.shiueh@gmail.com>
2016-08-17 11:59:25 +0800

    empty db data issue fix

--- pvi/h5/controller.py
+++ pvi/h5/controller.py
@@ -248,17 +248,19 @@ def pvi_query_info_energy_hourly_list():
     
     #-> insert zero energy value for missing hour
     dataset = []
-    dataset.append(info[0])
-    t_date = info[0][0]
-    i = 1
-    while i < len(info):
-        t_date = t_date + timedelta(hours=+1)
-        if t_date < info[i][0]:
-            dataset.append([t_date,0])
-        else:
-            dataset.append(info[i])
-            i += 1        
-    dataset.sort(key = lambda x: x[0])
+    if len(info) > 0:
+        dataset.append(info[0])
+        t_date = info[0][0]
+        i = 1
+        while i < len(info):
+            t_date = t_date + timedelta(hours=+1)
+            if t_date < info[i][0]:
+                dataset.append([t_date,0])
+            else:
+                dataset.append(info[i])
+                i += 1        
+        dataset.sort(key = lambda x: x[0])
+
     return dataset
 
 def pvi_query_info_energy_daily_list():

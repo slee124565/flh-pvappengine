6c5634d144189fc25c5192cdf66284f4fa3c573a
Lee Shiueh <lee.shiueh@gmail.com>
2016-09-08 17:48:57 +0800

    bug fix

--- pvappengine/cron_task.py
+++ pvappengine/cron_task.py
@@ -1,6 +1,6 @@
 #!/root/Env/appeng/bin/python
 
-import os, sys, django, logging
+import os, sys, django
 proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 print('pvappengine project root: %s' % proj_root)
@@ -13,20 +13,18 @@ from dbconfig.views import get_app_json_db_config
 import accuweather
 import pvi
     
-try:
-    from accuweather.models import CurrConditions
-    accu_dbconfig = get_app_json_db_config('accuweather', accuweather.DEFAULT_DB_CONFIG)
-    CurrConditions.save_current_location_condition(accu_dbconfig['locationkey'],
-                                                   accu_dbconfig['apikey'])
-    print('CurrConditions.save_current_location_condition executed')
-    
-    from pvi.views import save_all_pvi_input_register_value
-    save_all_pvi_input_register_value(get_app_json_db_config('pvi', pvi.DEFAULT_DB_CONFIG))
-    print('h5_controller.save_all_pvi_input_register_value executed')
+from accuweather.models import CurrConditions
+accu_dbconfig = get_app_json_db_config('accuweather', accuweather.DEFAULT_DB_CONFIG)
+CurrConditions.save_current_location_condition(accu_dbconfig['locationkey'],
+                                               accu_dbconfig['apikey'])
+print('CurrConditions.save_current_location_condition executed')
 
-    pvi.views.clear_expired_records()
-    accuweather.views.clear_expired_records()
+from pvi.views import save_all_pvi_input_register_value
+save_all_pvi_input_register_value(get_app_json_db_config('pvi', pvi.DEFAULT_DB_CONFIG))
+print('h5_controller.save_all_pvi_input_register_value executed')
 
-except:
-    print('except', exc_info=True)
+import accuweather.views
+pvi.views.clear_expired_records()
+accuweather.views.clear_expired_records()
+
 

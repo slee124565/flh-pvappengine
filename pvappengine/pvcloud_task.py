66e8d9f4f2fa80449d4588c84b88ef60ea984955
Lee Shiueh <lee.shiueh@gmail.com>
2016-09-20 14:32:00 +0800

    add pvs pvcloud_report function (v1)

---
+++ pvappengine/pvcloud_task.py
@@ -0,0 +1,52 @@
+from django.core import signing
+
+import logging
+logger = logging.getLogger(__name__)
+
+from dbconfig.models import AppOption
+from pi import get_pi_cpuinfo
+
+import requests
+import urllib
+
+PVCLOUD_URL = 'https://server-dot-solar-cloud-143410.appspot.com'
+
+def pvcloud_report_v1():
+    '''implement pvstation client report to pvcloud server function
+    post encrypted json data for pvcloud web api
+    ::
+    
+    {
+        'version': 'v1',
+        'cpuinfo': { 
+                    'hardware': 'xxx',
+                    'revision': 'xxx',
+                    'serial': 'xxx',
+                     },
+        'dbconfig' : {
+                    'pvi': '<json data>',
+                    'accuweather': '<json data>'
+                     },
+    }
+    '''
+    dbconfig = {}
+    entry = AppOption.objects.get(app_name = 'pvi')
+    dbconfig['pvi'] = entry.json_data
+    entry = AppOption.objects.get(app_name = 'accuweather')
+    dbconfig['accuweather'] = entry.json_data
+    
+    report_data = {'version': 'v1'}
+    report_data['cpuinfo'] = get_pi_cpuinfo()
+    report_data['dbconfig'] = dbconfig
+    
+    encrypt_report = signing.dumps(report_data)
+    
+    r = requests.post(PVCLOUD_URL,data={'data': encrypt_report})
+    logging.debug('pvcloud_report_v1 response status code %s and data\n%s' % (r.status_code,
+                                                                              r.text))
+    if r.status_code == 200:
+        return True
+    else:
+        logging.warning('pvcloud_report_v1 http post failed!')
+        return False
+    

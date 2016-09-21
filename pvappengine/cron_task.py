8269cb31eede6f2e13cb2788568b98c556acaad4
Lee Shiueh <lee.shiueh@gmail.com>
2016-09-21 23:10:14 +0800

    revise pvcloud task schedule

--- pvappengine/cron_task.py
+++ pvappengine/cron_task.py
@@ -1,6 +1,7 @@
 #!/root/Env/appeng/bin/python
 
 import os, sys, django
+import time, random
 proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 #print('pvappengine project root: %s' % proj_root)
@@ -19,11 +20,6 @@ CurrConditions.save_current_location_condition(accu_dbconfig['locationkey'],
                                                accu_dbconfig['apikey'])
 print('CurrConditions.save_current_location_condition executed')
 
-from pvappengine.pvcloud_task import pvcloud_report_v1, pvcloud_dbconfig_v1
-pvcloud_report_v1()
-pvcloud_dbconfig_v1()
-print('pvcloud_report executed')
-
 from pvi.views import save_all_pvi_input_register_value
 save_all_pvi_input_register_value(get_app_json_db_config('pvi', pvi.DEFAULT_DB_CONFIG))
 print('h5_controller.save_all_pvi_input_register_value executed')
@@ -31,5 +27,12 @@ print('h5_controller.save_all_pvi_input_register_value executed')
 import accuweather.views
 pvi.views.clear_expired_records()
 accuweather.views.clear_expired_records()
+
+from pvappengine.pvcloud_task import pvcloud_report_v1, pvcloud_dbconfig_v1
+time.sleep(random.randrange(1,10))
+pvcloud_report_v1()
+time.sleep(random.randrange(1,10))
+pvcloud_dbconfig_v1()
+print('pvcloud_report executed')
 
 
